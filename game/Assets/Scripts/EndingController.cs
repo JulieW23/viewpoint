849a15ee5c706c74e35f7e6b2e4292cbdd8e834f
Ting <ttk840719@gmail.com>
2018-04-03 15:21:56 -0400

    end scene added, connect to mainmenu

---
+++ game/Assets/Scripts/EndingController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class EndingController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Select")){
+			SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+		}
+	}
+}
