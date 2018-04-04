d9562a3d2d948a7d0211bad770473c26547b30ae
Ting <ttk840719@gmail.com>
2018-04-04 10:22:08 -0400

    change level 3 text

--- game/Assets/Scripts/RotationPad.cs
+++ game/Assets/Scripts/RotationPad.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 public class RotationPad : MonoBehaviour {
 
 	public bool rotatable = false;
-		
+	public GameObject YButton;
 	void OnCollisionStay(Collision collision){
 		if (collision.gameObject.tag == "Player") {
 			rotatable = true;
+			YButton.SetActive(true);
 		}
 	}
 
 	void OnCollisionExit(Collision collision){
 		if (collision.gameObject.tag == "Player") {
 			rotatable = false;
+			YButton.SetActive(false);
 		}
 	}
 }
